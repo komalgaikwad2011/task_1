da5d3aba938ec857d9d29842acc964714e545aef
Komal Gaikwad <163570757+komalgaikwad2011@users.noreply.github.com>
2026-01-15 18:42:41 +0530

    Add files via upload

--- task.java
+++ task.java
@@ -2,10 +2,15 @@ package komal1;
 
 public class task {
 
-	public static void main(String[] args)
-	{
-		System.out.println("hello world...");
+	    public static void main(String[] args) 
+	    {
 
-	}
+	        System.out.println("Java Developer Internship Task");
 
+	        System.out.println("Command Line Arguments:");
+	        for (String arg : args) 
+	        {
+	            System.out.println(arg);
+	        }
+	    }
 }
